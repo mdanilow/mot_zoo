ea4a3cfe215a45db1c6cc6b08bbc5150fbb36974
danilowi <danilowi@agh.edu.pl>
2025-06-03 14:05:33 +0200

    tracker repo name changed, sort results reproduced with base_cort.json config

--- scripts/run_tracker_and_mot_eval.sh
+++ scripts/run_tracker_and_mot_eval.sh
@@ -1,9 +1,13 @@
 # DETECTIONS="quantyolov8_4w4a_mot15_cl0"
-DETECTIONS="floatyolov8n_640_cl0"
-cd sort
-# python sort.py --seq_path "../yolo_finn/runs/detect" --phase ${DETECTIONS}
-python sort.py --seq_path "../yolo_finn/runs/detect" --phase ${DETECTIONS} --use_conv_features 4 --debug_images /media/vision/1d6890f4-df75-4531-a044-f6d3d44d033d/Downloads/MOT15/train
+# DETECTIONS="floatyolov8n_640_cl0"
+DETECTIONS="sortdets"
+NAME="base_sort"
+CONFIG="configs/base_sort.json"
+# SINGLE_SEQUENCE="ETH-Sunnyday"
+SINGLE_SEQUENCE="None"
+cd dcf_mot
+python sort.py --seq_path "../yolo_finn/runs/detect" --single_sequence ${SINGLE_SEQUENCE} --name ${NAME} --phase ${DETECTIONS} --config ${CONFIG} --debug_images /media/vision/1d6890f4-df75-4531-a044-f6d3d44d033d/Downloads/MOT15/train
 
 cd ../TrackEval
-TRACKERS_FOLDER=../sort/output
-python scripts/run_mot_challenge.py --SKIP_SPLIT_FOL True --METRICS CLEAR --BENCHMARK MOT15 --TRACKERS_FOLDER ${TRACKERS_FOLDER} --TRACKERS_TO_EVAL ${DETECTIONS}
+TRACKERS_FOLDER=../dcf_mot/output
+python scripts/run_mot_challenge.py --SINGLE_SEQUENCE ${SINGLE_SEQUENCE} --SKIP_SPLIT_FOL True --METRICS CLEAR --BENCHMARK MOT15 --TRACKERS_FOLDER ${TRACKERS_FOLDER} --TRACKERS_TO_EVAL "${DETECTIONS}_${NAME}"
--- scripts/runtracker.sh
+++ scripts/runtracker.sh
@@ -1,5 +1,8 @@
 # DETECTIONS="quantyolov8_4w4a_mot15_cl0"
-DETECTIONS="floatyolov8n_640_cl0"
-cd sort
-python sort.py --seq_path "../yolo_finn/runs/detect" --phase ${DETECTIONS} --use_conv_features 0 --debug_images /media/vision/1d6890f4-df75-4531-a044-f6d3d44d033d/Downloads/MOT15/train --debug
-# python sort.py --seq_path "../yolo_finn/runs/detect" --phase ${DETECTIONS}
+# DETECTIONS="floatyolov8n_640_cl0"
+DETECTIONS="mot15dets"
+NAME="test"
+CONFIG="configs/test.json"
+SINGLE_SEQUENCE="ETH-Sunnyday"
+cd dcf_mot
+python sort.py --debug --seq_path "../yolo_finn/runs/detect" --single_sequence ${SINGLE_SEQUENCE} --name ${NAME} --phase ${DETECTIONS} --config ${CONFIG} --debug_images /media/vision/1d6890f4-df75-4531-a044-f6d3d44d033d/Downloads/MOT15/train
